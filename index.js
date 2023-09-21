48d19b5303a5fc7b0844087db7b0060d88bafe36
ksenofonus <danilova.ka88@yandex.ru>
2023-09-21 23:24:48 +0400

    feat: add prev and next function without toggle

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 let isPlay = false;
 let playNum = 0;
-let songsList = [];
+let playList = ['./assets/audio/beyonce (1).mp3', './assets/audio/dontstartnow.mp3'];
 const audio = document.querySelector('audio');
 const playButton = document.querySelector('.play-button');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+//play
 playButton.addEventListener('click', function playPause() {
   if (!isPlay) {
     // audio.currentTime = 0;
@@ -11,6 +15,40 @@ playButton.addEventListener('click', function playPause() {
   } else {
     audio.pause();
     playButton.classList.remove('pause');
+  }
+  isPlay = !isPlay;
+});
+
+//prev
+prevButton.addEventListener('click', function prev() {
+  playNum = playNum - 1;
+  if (playNum < 0) {
+    playNum = 1; //playList.length - 1
+  } else if (playNum > 1) {
+    playNum = 0;
+  }
+  audio.setAttribute('src', playList[playNum]);
+  if (!isPlay) {
+    // audio.currentTime = 0;
+    audio.play();
+    playButton.classList.add('pause');
+  }
+  isPlay = !isPlay;
+});
+
+//next
+nextButton.addEventListener('click', function next() {
+  playNum = playNum + 1;
+  if (playNum < 0) {
+    playNum = 1;
+  } else if (playNum > 1) {
+    playNum = 0;
+  }
+  audio.setAttribute('src', playList[playNum]);
+  if (!isPlay) {
+    // audio.currentTime = 0;
+    audio.play();
+    playButton.classList.add('pause');
   }
   isPlay = !isPlay;
 });
