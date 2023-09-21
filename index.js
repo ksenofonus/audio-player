1364f45375fd101d19ed4de1eef70c0facc7660a
ksenofonus <danilova.ka88@yandex.ru>
2023-09-21 22:35:52 +0400

    feat: change play button tag and add change for play button on click

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ let isPlay = false;
 let playNum = 0;
 let songsList = [];
 const audio = document.querySelector('audio');
-const playButton = document.querySelector('.play');
+const playButton = document.querySelector('.play-button');
 playButton.addEventListener('click', function playPause() {
   if (!isPlay) {
     // audio.currentTime = 0;
     audio.play();
+    playButton.classList.add('pause');
   } else {
     audio.pause();
+    playButton.classList.remove('pause');
   }
   isPlay = !isPlay;
 });
-
-// playButton.addEventListener('click', () => console.log('click'));
