fc419b2a8e61a16fb323a7918d82aa57dce2658e
ksenofonus <danilova.ka88@yandex.ru>
2023-09-22 09:20:53 +0400

    feat: add change title song

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 let isPlay = false;
 let playNum = 0;
 let playList = ['./assets/audio/beyonce (1).mp3', './assets/audio/dontstartnow.mp3'];
+let coverList = ['./assets/images/lemonade.png', './assets/images/dontstartnow.png'];
+let singerList = ['Beyonce', 'Dua Lipa'];
+let titleList =['Don\'t hurt yourself', 'Don\'t stop now'];
+
 const audio = document.querySelector('audio');
 const playButton = document.querySelector('.play-button');
 const prevButton = document.querySelector('.prev');
 const nextButton = document.querySelector('.next');
+const background = document.querySelector('.background');
+const cover = document.querySelector('.audio__cover img');
+const singer = document.querySelector('.singer');
+const song = document.querySelector('.song');
+
 
 //play
 playButton.addEventListener('click', function playPause() {
@@ -28,13 +37,15 @@ prevButton.addEventListener('click', function prev() {
     playNum = 0;
   }
   audio.setAttribute('src', playList[playNum]);
-  // if (!isPlay) {
+  background.setAttribute('src', coverList[playNum]);
+  cover.setAttribute('src', coverList[playNum]);
+  singer.innerHTML = singerList[playNum];
+  song.innerHTML = titleList[playNum];
     // audio.currentTime = 0;
     audio.play();
     isPlay = !isPlay;
     playButton.classList.add('pause');
-  // }
-});
+  });
 
 //next
 nextButton.addEventListener('click', function next() {
@@ -45,10 +56,12 @@ nextButton.addEventListener('click', function next() {
     playNum = 0;
   }
   audio.setAttribute('src', playList[playNum]);
-  // if (!isPlay) {
+  background.setAttribute('src', coverList[playNum]);
+  cover.setAttribute('src', coverList[playNum]);
+  singer.innerHTML =singerList[playNum];
+  song.innerHTML= titleList[playNum];
     // audio.currentTime = 0;
     audio.play();
-    isPlay = !isPlay;
+    isPlay = isPlay;
     playButton.classList.add('pause');
-  // }
 });
