1e2b2de27bceb95d24b99958f342f9cf1d38dea6
ksenofonus <danilova.ka88@yandex.ru>
2023-09-22 14:43:29 +0400

    feat: add new mp3 files and album  covers

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let isPlay = false;
 let playNum = 0;
-let playList = ['./assets/audio/beyonce (1).mp3', './assets/audio/dontstartnow.mp3'];
-let coverList = ['./assets/images/lemonade.png', './assets/images/dontstartnow.png'];
-let singerList = ['Beyonce', 'Dua Lipa'];
-let titleList =['Don\'t hurt yourself', 'Don\'t stop now'];
+let playList = ['./assets/audio/beyonce (1).mp3', './assets/audio/dontstartnow.mp3', './assets/audio/Cheek_to_Cheek.mp3', './assets/audio/Go_Down_Moses.mp3', './assets/audio/What_A_Wonderful_World.mp3'];
+let coverList = ['./assets/images/lemonade.png', './assets/images/dontstartnow.png', './assets/images/cheek-to-cheek.png', './assets/images/go_down.png', './assets/images/wonderfull_world.png'];
+let singerList = ['Beyonce', 'Dua Lipa', 'Louis Armstrong', 'Louis Armstrong', 'Louis Armstrong'];
+let titleList =['Don\'t hurt yourself', 'Don\'t stop now', 'Cheek to Cheek', 'Go Down Moses', 'What A Wonderful World'];
 
 const audio = document.querySelector('audio');
 const playButton = document.querySelector('.play-button');
@@ -36,8 +36,8 @@ playButton.addEventListener('click', function playPause() {
 prevButton.addEventListener('click', function prev() {
   playNum = playNum - 1;
   if (playNum < 0) {
-    playNum = 1; //playList.length - 1
-  } else if (playNum > 1) {
+    playNum = playList.length - 1; //
+  } else if (playNum > (playList.length - 1)) {
     playNum = 0;
   }
   audio.setAttribute('src', playList[playNum]);
@@ -55,8 +55,8 @@ prevButton.addEventListener('click', function prev() {
 nextButton.addEventListener('click', function next() {
   playNum = playNum + 1;
   if (playNum < 0) {
-    playNum = 1;
-  } else if (playNum > 1) {
+    playNum = playList.length - 1;
+  } else if (playNum > (playList.length - 1)) {
     playNum = 0;
   }
   audio.setAttribute('src', playList[playNum]);
